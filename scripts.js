251f371aa05588e05a6eb3ef12d96173dd65af79
Farid García <farid.herrera@gmail.com>
2017-10-14 12:57:17 -0500

    Multiplica v2

--- scripts.js
+++ scripts.js
@@ -1,7 +1,27 @@
+var colores,
+els = ['header','.left','.right','footer'];
+
 $(function() {
-	var colores;
 	$.getJSON("colores.json", function (data) {
-		colores = data;
+		colores = data.colors;
+		$('.switch').click(function(){
+			colores = shuffle(colores);
+			for (i = 0; i < els.length; i++) {
+				$(els[i]).css({'background-color': colores[i].code.hex });
+			}
+		});
+		
 	});
-	console.log(colores);
 });
+
+function shuffle(array) {
+  var currIndex = array.length, tempVal, randomIndex;
+  while (0 !== currIndex) {
+  	randomIndex = Math.floor(Math.random() * currIndex);
+  	currIndex -= 1;
+  	tempVal = array[currIndex];
+    array[currIndex] = array[randomIndex];
+    array[randomIndex] = tempVal;
+  }
+  return array;
+}
